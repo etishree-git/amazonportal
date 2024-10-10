20ed8a1b9a57cf832bbcdfffabd46fe1909d14cb
etishree-git <etishreebehera281@gmail.com>
2024-10-10 07:55:23 +0000

    merge conflict resolved

--- DBUtils.java
+++ DBUtils.java
@@ -2,7 +2,11 @@ Hello guys..
 This file contains db relared code
 Hello added this line.
 hello adding dev branch.
+<<<<<<< HEAD
 Hello added this line
 Hello added this line
 hello adding dev branch here.
 hello added this line for git cherry-pick.
+=======
+hello added this line for git cherry-pick
+>>>>>>> 0c19999 (updated)
