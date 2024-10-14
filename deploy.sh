dbbf3b89fc431edfa21e1c375e22279db9572731
etishree-git <etishreebehera281@gmail.com>
2024-10-14 06:46:18 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ Hello updating in dev branch.
 hello i am updating in stage branch innn....
 Hello i am update in stage branch..
 Hello updated this line..
-hello updated in stage file.
+hiiiiiiiiii.....
 hi i am updating in stasge branch.
