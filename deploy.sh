f345994820d0132b6d3ef449e92df0104c26d88f
etishree-git <etishreebehera281@gmail.com>
2024-10-14 06:12:02 +0000

    merge conflict resolved

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@ Hello the file contains the deployement script.
 Hello updating in master branch..
 Hello updating in dev branch.
 hello i am updating in stage branch innn....
+Hello i am update in stage branch..
+Hello updated this line..
+hello updated in stage file.
+hi i am updating in stasge branch.
